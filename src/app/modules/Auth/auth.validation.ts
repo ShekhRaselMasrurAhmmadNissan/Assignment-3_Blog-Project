3933aa02e5f11ee499c6494ea64c83683cf5dfbc
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2025-02-12 12:55:46 +0600

    FEAT: Auth Validation Schema added.

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -1,2 +1,22 @@
+import { z } from 'zod';
 
-// Define your validations here
+const loginUserValidationSchema = z.object({
+	body: z.object({
+		email: z
+			.string({
+				required_error: 'Email is required',
+				invalid_type_error: 'Email must be a string',
+			})
+			.email({
+				message: 'Please Enter a valid Email.',
+			}),
+	}),
+	password: z.string({
+		required_error: 'Password is required',
+		invalid_type_error: 'Password must be a string',
+	}),
+});
+
+export const AuthValidation = {
+	loginUserValidationSchema,
+};
