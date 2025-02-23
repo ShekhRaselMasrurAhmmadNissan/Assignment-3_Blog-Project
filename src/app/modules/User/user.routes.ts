feeb5799a6f402e96346e8e09e01b81345fee08c
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2025-02-23 09:55:59 +0600

    FEAT: Implement user routes with validation and CRUD operations

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,2 +1,24 @@
+import express from 'express';
+import validateRequests from '../../middleware/validateRequests';
+import { UserControllers } from './user.controller';
+import { UserValidation } from './user.validation';
 
-// Define your routes here
+const router = express.Router();
+
+router
+	.route('/')
+	.post(
+		validateRequests(UserValidation.createUserValidationSchema),
+		UserControllers.createUser
+	)
+	.get(UserControllers.getAllUser);
+
+router
+	.route('/:id')
+	.get(UserControllers.getSingleUser)
+	.patch(UserControllers.updateUser)
+	.delete(UserControllers.deleteUser);
+
+router.route('/block/:id').patch(UserControllers.blockUser);
+
+export const UserRoutes = router;
